e0c150c49e8a8431983bda275b10fd263f1546c4
Rishib#321 <rishitab2209@gmail.com>
2026-06-24 00:06:21 +0530

    added field for refresh token

--- auth_service/src/main/java/com/ecommerce/auth_service/dto/AuthResponse.java
+++ auth_service/src/main/java/com/ecommerce/auth_service/dto/AuthResponse.java
@@ -11,8 +11,8 @@ import lombok.*;
 @Builder
 public class AuthResponse {
 
-    private String token;
-
+    private String accessToken;
+    private String refreshToken;
     private String message;
 
 }
